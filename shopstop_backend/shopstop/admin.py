8d3203397a131c9378f874aab6906eba6c821a71
Tobias Kongsvik <tokongs98@gmail.com>
2020-02-14 16:27:14 +0100

    Add ListItem model
    * Add a ListItem model
    * Add test for the custom save method
    * Add an admin panel for the ListItem

--- shopstop_backend/shopstop/admin.py
+++ shopstop_backend/shopstop/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import List
+from .models import List, ListItem
 
 
 class ListAdmin(admin.ModelAdmin):
     pass
 
 
+class ListItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'bought', 'list',)
+    list_filter = ('bought', 'list__name', 'list__group__name')
+    search_fields = ('name', 'list__name')
+
+
 admin.site.register(List, ListAdmin)
+admin.site.register(ListItem, ListItemAdmin)
