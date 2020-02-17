0e361605e8a61725e93723100417bf05fdaf7849
Håvard Pettersen <haavapet@stud.ntnu.no>
2020-02-17 17:38:11 +0100

    remove mock snapshot test because its probably a bad idea and i can just remove the test i had to get it to pass

--- shopstop-frontend/src/screens/SingleListScreen.tsx
+++ shopstop-frontend/src/screens/SingleListScreen.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { RouteProp } from '@react-navigation/native';
 import List from '../components/List';
 
-const SingleList = ({ route }) => {
-    return <List route={route} />;
+// These lines below makes a new type for the route prop which we use in interface.
+type RootStackParamList = {
+    list: { id: string };
+};
+type ProfileScreenRouteProp = RouteProp<RootStackParamList, 'list'>;
+
+export interface SingleListInterface {
+    route: ProfileScreenRouteProp;
+}
+
+const SingleList: React.FC<SingleListInterface> = props => {
+    return <List id={props.route.params.id} />;
 };
 
 export default SingleList;
