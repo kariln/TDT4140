360066f620dc6540681f6824bbdf920e2869a7d2
Tobias Kongsvik <tokongs98@gmail.com>
2020-02-26 21:25:40 +0100

    Add group endpoints and permissions
    * Add django guardian
    * Add ModelViewset for Group
    * Resrict access to List and ListItems based on group
    * Restrict access to Group

--- shopstop_backend/shopstop/serializers.py
+++ shopstop_backend/shopstop/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import Group
+from guardian.shortcuts import assign_perm
 from rest_framework import serializers
 
 from .models import List, ListItem
@@ -6,26 +8,53 @@ from .models import List, ListItem
 class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
+        read_only_fields = ['created_at', 'modified_at']
         fields = [
             'name',
             'group',
-            'created_at',
-            'modified_at'
         ]
 
     def create(self, validated_data):
-        return List(**validated_data)
+        return List.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.group = validated_data.get('group', instance.group)
-        instance.created_at = validated_data.get('created_at', instance.created_at)
-        instance.modified_at = validated_data.get('modified_at', instance.modified_at)
         instance.save()
         return instance
 
 
 class ListItemSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        return ListItem.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.group = validated_data.get('quantity', instance.quantity)
+        instance.save()
+        return instance
+
     class Meta:
         model = ListItem
         fields = ['id', 'name', 'quantity', 'bought', 'list']
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        group = Group.objects.create(**validated_data)
+        assign_perm('view_group', self.context['user'], group)
+        assign_perm('change_group', self.context['user'], group)
+        assign_perm('add_group', self.context['user'], group)
+        assign_perm('delete_group', self.context['user'], group)
+        return group
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+    class Meta:
+        model = Group
+        fields = ['id', 'name']
--- shopstop_backend/shopstop/urls.py
+++ shopstop_backend/shopstop/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
 
-from .views import ListItemViewSet, ListViewSet
+from .views import GroupViewSet, ListItemViewSet, ListViewSet
 
 router = routers.DefaultRouter()
 router.register('lists', ListViewSet)
 router.register('list-items', ListItemViewSet)
+router.register('groups', GroupViewSet)
 urlpatterns = router.urls
